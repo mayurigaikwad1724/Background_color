d91c689b06d30be8fbd21fcce7b51159d0363b2b
Mayuri Gaikwad <76725453+mayurigaikwad1724@users.noreply.github.com>
2023-07-16 14:14:42 +0530

    Add files via upload

---
+++ color_botton.js
@@ -0,0 +1,15 @@
+const box=document.getElementById("Box");
+const red_button=document.getElementById("red");
+red_button.onclick=function(){
+    Box.style.backgroundColor="red";
+}
+const yellow_button=document.getElementById("yellow");
+yellow_button.onclick=function(){
+    Box.style.backgroundColor="yellow";
+}
+const green_button=document.getElementById("green");
+green_button.onclick=function(){
+    Box.style.backgroundColor="green";
+}
+
+
